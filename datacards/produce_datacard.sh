654f08f7ed015c5f3f2be2ff4ef2ba3810423a5d
Moritz Scham <moritz.scham@student.kit.edu>
2019-07-25 15:11:34 +0200

    add ff training sample generation

--- datacards/produce_datacard.sh
+++ datacards/produce_datacard.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 source utils/setup_cmssw.sh
 source utils/setup_python.sh
 
@@ -8,15 +8,31 @@ STXS_SIGNALS=$2
 CATEGORIES=$3
 JETFAKES=$4
 EMBEDDING=$5
-CHANNELS=${@:6}
+TRAINING_METHOD=$6
+CHANNELS=${@:7}
 
 NUM_THREADS=8
+    if [[ $TRAINING_METHOD  =~ "emb" ]]; then
+        TRAIN_EMB=1
+    else
+        TRAIN_EMB=0
+    fi
+    if [[ $TRAINING_METHOD =~ "ff" ]]; then
+        TRAIN_FF=1
+    else
+        TRAIN_FF=0
+    fi
+
 
 # Remove output directory
 rm -rf output/${ERA}_smhtt
 
+trap 'exit $?' TRAP KILL EXIT
+
+(
+set -x
 # Create datacards
-$CMSSW_BASE/bin/slc6_amd64_gcc530/MorphingSM2017 \
+$CMSSW_BASE/bin/slc7_amd64_gcc530/MorphingSM2017 \
     --base_path=$PWD \
     --input_folder_mt="/" \
     --input_folder_et="/" \
@@ -33,7 +49,10 @@ $CMSSW_BASE/bin/slc6_amd64_gcc530/MorphingSM2017 \
     --stxs_signals=$STXS_SIGNALS \
     --categories=$CATEGORIES \
     --era=$ERA \
-    --output="${ERA}_smhtt"
+    --output="${ERA}_smhtt" \
+    --train_ff $TRAIN_FF \
+    --train_emb $TRAIN_EMB
+)
 
 # Use Barlow-Beeston-lite approach for bin-by-bin systematics
 THIS_PWD=${PWD}
